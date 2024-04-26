8d263520ab1fa6c8730dca37118ebee839b88779
Tony Rogvall <tony@rogvall.se>
2024-04-26 14:53:02 +0200

    more utf / utf8 fixes and options

--- priv/ej.js
+++ priv/ej.js
@@ -38,20 +38,18 @@ function EiClass() {
     this.LARGE_TUPLE = 105;
     this.NIL = 106;
     this.MAP = 116;
-    this.ATOM_UTF8 = 115;        
+    this.ATOM_UTF8 = 118;        
     this.SMALL_ATOM_UTF8 = 119;    
-    
+
+    // Encoding options
     this.use_map = false;         // use map when available
-    this.use_small_atom = true;   // use small atom when available
     
-    if (this.use_small_atom) {
-	this.true_atom_size  = 1+4;
-	this.false_atom_size = 1+5;
-    }
-    else {
-	this.true_atom_size  = 2+4;
-	this.false_atom_size = 2+5;
-    }
+    this.encode_utf8_atom = true;       // encode atoms as utf8
+    this.encode_unicode_strings = true; // encode string as 16-bit code points
+    this.encode_utf8_strings = false;    // encode string as utf8 bytes
+    this.decode_string_utf8 = true;  // decode list as unicode string
+    this.decode_list_string = true;  // try decode list as string
+    this.decode_list_utf8 = true;    // try decode list as utf8 string
 };
 
 //
@@ -143,10 +141,122 @@ EiClass.prototype.isBinary = function (Obj) {
     return (Obj.type === "Binary");
 };
 
-EiClass.prototype.binary_size = function (Obj) {
-    if (Obj.type === "Binary") return Obj.value.byteLength;
+EiClass.prototype.binary_size = function (obj) {
+    if (obj.type === "Binary") return obj.value.byteLength;
     return 0;
 };
+
+EiClass.prototype.is_latin1_string = function (str) {
+    var len = str.length;
+    var i = 0;
+    while(i < len) {
+	var x = str.charCodeAt(i);
+	if (x >= 256) return false;
+	i++;
+    }
+    return true;
+}
+
+// calculate number of utf8 bytes needed to encode a string
+EiClass.prototype.string_utf8_length = function (str) {
+    var len = str.length;
+    var i = len - 1;
+    while(i >= 0) {
+	var x = str.charCodeAt(i);
+	if ((x > 0x7f) && (x <= 0x7ff))
+	    len++;
+	else if ((x > 0x7ff) && (x <= 0xffff))
+	    len += 2;
+	if ((x >= 0xdc00) && (x <= 0xdfff)) i--; //trail surrogate
+	i--;
+    }
+    return len;
+}
+
+// write string (16-bit) as utf8 bytes
+EiClass.prototype.write_utf8_bytes = function (str,dv,pos,count) {
+    var i = 0;
+    while(i < count) {
+	var x = str.charCodeAt(i);
+	if (x < 256)
+	    dv.setUint8(pos++, x);
+	else if (x < 2048) {
+	    dv.setUInt8(pos++, 0xf0 | (x >> 6));
+	    dv.setUInt8(pos++, 0x80 | (x & 0x3f));
+	}
+	else {
+	    x &= 0xFFFF; // truncate to 16 bits
+	    dv.setUInt8(pos++, 0xe0 | (x >> 12));
+	    dv.setUInt8(pos++, 0x80 | ((x >> 6) & 0x3f));
+	    dv.setUInt8(pos++, 0x80 | (x & 0x3f));
+	}
+	i++;
+    }
+    return pos;
+};
+
+// write string (16-bit) as list of utf8 bytes
+EiClass.prototype.write_utf8_list = function (str,dv,pos,count) {
+    var i = 0;
+    while (i < count) {
+	x = Obj.charCodeAt(i);
+	if (x <= 255) {
+	    dv.setUint8(pos++, this.SMALL_INTEGER);
+	    dv.setUint8(pos++, x);
+	}
+	else if (x < 2048) {
+	    dv.setUint8(pos++, this.SMALL_INTEGER);
+	    dv.setUInt8(pos++, 0xf0 | (x >> 6));
+	    dv.setUint8(pos++, this.SMALL_INTEGER);
+	    dv.setUInt8(pos++, 0x80 | (x & 0x3f));
+	}
+	else {
+	    x &= 0xFFFF; // truncate to 16 bits
+	    dv.setUint8(pos++, this.SMALL_INTEGER);
+	    dv.setUInt8(pos++, 0xe0 | (x >> 12));
+	    dv.setUint8(pos++, this.SMALL_INTEGER);
+	    dv.setUInt8(pos++, 0x80 | ((x >> 6) & 0x3f));
+	    dv.setUint8(pos++, this.SMALL_INTEGER);		    
+	    dv.setUInt8(pos++, 0x80 | (x & 0x3f));
+	}
+	i++;
+    }
+    return pos;
+}
+
+EiClass.prototype.write_latin1_bytes = function (str,dv,pos,count) {
+    var i = 0;
+    while(i < count) {
+	var x = str.charCodeAt(i);
+	dv.setUint8(pos++, x & 0xff);
+	i++;
+    }
+    return pos;
+};
+
+EiClass.prototype.write_list_bytes = function (str,dv,pos,count) {
+    var i = 0;
+    while(i < count) {
+	var x = str.charCodeAt(i);
+	dv.setUint8(pos++, this.SMALL_INTEGER);
+	dv.setUint8(pos++, x & 0xff);
+	i++;
+    }
+    return pos;
+};
+
+EiClass.prototype.write_list_integers = function (str,dv,pos,count) {
+    var i = 0;
+    while(i < count) {
+	var x = str.charCodeAt(i);
+	dv.setUint8(pos++, this.INTEGER);
+	dv.setInt32(pos+1, x);
+	pos += 4;
+	i++;
+    }
+    return pos;
+};
+
 
 EiClass.prototype.atom = function (Obj) {
     return new EiAtom(Obj);
@@ -176,14 +286,37 @@ EiClass.prototype.byte_size_term = function (Obj) {
     var len, Size;
     switch(typeof(Obj)) {
     case 'string':
-	len = Obj.length;
-	if (len <= 65535) 
-	    Size = 2+len;      // STRING encoded
-	else
-	    Size = 4+2*len+1;  // LIST encoded (with terminating nil)!
+	if (this.encode_unicode_strings) {
+	    len = Obj.length;
+	    if (this.is_latin1_string(Obj)) {
+		if (len < 65535)
+		    Size = 2+len; // STRING encoded
+		else 
+		    Size = 4+2*len+1; // LIST encoded (SMALL_INTEGER)
+	    }
+	    else
+		Size = 4+5*len+1; // LIST encoded (INTEGER)
+	}
+	else if (this.encode_utf8_strings) {
+	    len = this.string_utf8_length(Obj); // utf8 length
+	    if (len <= 65535) 
+		Size = 2+len;      // STRING encoded (SMALL_INTEGER)
+	    else
+		Size = 4+2*len+1;  // LIST encoded (SMALL_INTEGER)
+	}
+	else { // encode as (truncated) latin1
+	    len = Obj.length;
+	    if (len < 65535)
+		Size = 2+len; // STRING encoded
+	    else
+		Size = 4+2*len+1; // LIST encoded (SMALL_INTEGER)
+	}
 	break;
     case 'boolean':
-	Size = Obj ? this.true_atom_size : this.false_atom_size;
+	if (Obj)
+	    Size = 1+4; // "true"
+	else
+	    Size = 1+5; // "false
 	break;
     case 'number':
 	if (Obj % 1 === 0) { // then integer
@@ -201,7 +334,7 @@ EiClass.prototype.byte_size_term = function (Obj) {
     case 'object':
 	switch (Obj.type) {
 	case 'Atom':
-	    Size = this.atom_size(Obj.value);
+	    Size = this.atom_size(Obj.value); // FIXME utf8_size
 	    break;
 	case 'Binary':
 	    Size = 4 + Obj.value.byteLength;
@@ -226,11 +359,17 @@ EiClass.prototype.byte_size_term = function (Obj) {
     return 1+Size;
 }
 
-EiClass.prototype.atom_size = function (Name) {
-    if ((Name.length < 256) && this.use_small_atom)
-	return 1+Name.length;
+
+EiClass.prototype.atom_size = function (name) {
+    var len;
+    if (this.encode_utf8_atom)
+	len = this.string_utf8_length(name);
     else
-	return 2+Name.length;
+	len = name.length;
+    if (len < 256)
+	return len+1;
+    else
+	return len+2;
 }
 
 EiClass.prototype.byte_size_reference = function (Obj) {
@@ -311,31 +450,94 @@ EiClass.prototype.encode = function (Obj) {
 };
 
 EiClass.prototype.encode_term = function (Obj,dv,pos) {
+    var len;
     switch(typeof(Obj)) {
     case 'string':
-	if (Obj.length <= 65535) {
-	    dv.setUint8(pos++, this.STRING);
-	    dv.setUint16(pos, Obj.length);
-	    pos += 2;
-	    pos = this.string_to_bytes(Obj,dv,pos,Obj.length);
+	if (this.encode_unicode_strings) {
+	    len = Obj.length;
+	    if (this.is_latin1_string(Obj)) {
+		if (len < 65535) {
+		    dv.setUint8(pos++, this.STRING);
+		    dv.setUint16(pos, len);
+		    pos += 2;
+		    pos = this.write_latin1_bytes(Obj,dv,pos,len);
+		}
+		else {
+		    dv.setUint8(pos++, this.LIST);
+		    dv.setUint32(pos, len, false);
+		    pos += 4;
+		    pos = this.write_list_bytes(Obj,dv,pos,len);
+		    dv.setUint8(pos++, this.NIL);
+		}
+	    }
+	    else {
+		dv.setUint8(pos++, this.LIST);
+		dv.setUint32(pos, len, false);
+		pos += 4;
+		pos = this.write_list_integers(Obj,dv,pos,len);
+		dv.setUint8(pos++, this.NIL);		
+	    }
+	}
+	else if (this.encode_utf8_strings) {
+	    len = this.string_utf8_length(Obj);
+	    if (len <= 65535) {
+		dv.setUint8(pos++, this.STRING);
+		dv.setUint16(pos, len);
+		pos += 2;
+		pos = this.write_utf8_bytes(Obj,dv,pos,Obj.length);
+	    }
+	    else {
+		var i, l;
+		dv.setUint8(pos++, this.LIST);
+		dv.setUint32(pos, len);
+		pos += 4;
+		pos = this.write_utf8_list(Obj,dv,pos,Obj.length);
+		dv.setUint8(pos++, this.NIL);
+	    }
 	}
 	else {
-	    dv.setUint8(pos++, this.LIST);
-	    dv.setUint32(pos, Obj.length);
-	    pos += 4;
-	    for (i = 0; i < Obj.length; i++) {
-		dv.setUint8(pos++, this.SMALL_INTEGER);
-		dv.setUint8(pos++, Obj.charCodeAt(i));
+	    len = Obj.length;
+	    if (len < 65535) {
+		dv.setUint8(pos++, this.STRING);
+		dv.setUint16(pos, len);
+		pos += 2;
+		pos = this.write_latin1_bytes(Obj,dv,pos,len);
 	    }
-	    dv.setUint8(pos++, this.NIL);
+	    else {
+		dv.setUint8(pos++, this.LIST);
+		dv.setUint32(pos, len, false);
+		pos += 4;
+		pos = this.write_list_bytes(Obj,dv,pos,len);
+	    }
 	}
 	break;
 
     case 'boolean':
-	if (Obj)
-	    pos = this.encode_atom_string("true",dv,pos);
-	else 
-	    pos = this.encode_atom_string("false",dv,pos);
+	if (Obj) {
+	    if (this.encode_utf8_atom)
+		dv.setUint8(pos, this.SMALL_ATOM_UTF8);
+	    else
+		dv.setUint8(pos, this.SMALL_ATOM);
+	    dv.setUint8(pos+1, 4);
+	    dv.setUint8(pos+2, 't'.charCodeAt(0));
+	    dv.setUint8(pos+3, 'r'.charCodeAt(0));
+	    dv.setUint8(pos+4, 'u'.charCodeAt(0));
+	    dv.setUint8(pos+5, 'e'.charCodeAt(0));
+	    pos += 6;
+	}
+	else {
+	    if (this.encode_utf8_atom)	    
+		dv.setUint8(pos, this.SMALL_ATOM_UTF8);
+	    else
+		dv.setUint8(pos, this.SMALL_ATOM);
+	    dv.setUint8(pos+1, 5);
+	    dv.setUint8(pos+2, 'f'.charCodeAt(0));
+	    dv.setUint8(pos+3, 'a'.charCodeAt(0));
+	    dv.setUint8(pos+4, 'l'.charCodeAt(0));
+	    dv.setUint8(pos+5, 's'.charCodeAt(0));
+	    dv.setUint8(pos+6, 'e'.charCodeAt(0));
+	    pos += 7;
+	}
 	break;
 
     case 'number':
@@ -450,24 +652,40 @@ EiClass.prototype.encode_term = function (Obj,dv,pos) {
     return pos;
 };
 
-EiClass.prototype.encode_atom_string = function (Name,dv,pos) {
-    var len = Name.length;
-    var i;
-    // fix length should be number of bytes in utf8 encoding
-    // now characters are truncated to 7 bits!
-    if ((len < 256) && this.use_small_atom) {
-	dv.setUint8(pos, this.SMALL_ATOM_UTF8);
-	dv.setUint8(pos+1, len);
-	pos += 2;
+EiClass.prototype.encode_atom_string = function (name,dv,pos) {
+    var len;
+
+    if (this.encode_utf8_atom) {
+	len = this.string_utf8_length(name);
+	if (len < 256) {
+	    dv.setUint8(pos++, this.SMALL_ATOM_UTF8);
+	    dv.setUint8(pos++, len);
+	}
+	else if (len < 65536) {
+	    dv.setUint8(pos++, this.ATOM_UTF8);
+	    dv.setUint16(pos, len, false);
+	    pos += 2;
+	}	
+	else
+	    throw("bad atom: too big");    
+	pos = this.write_utf8_bytes(name,dv,pos,name.length);
     }
-    else if (len < 65536) {
-	dv.setUint8(pos, this.ATOM_UTF8);
-	dv.setUint16(pos+1, len, false);
-	pos += 3;
+    else {  // use latin1 encoding 
+	len = name.length;
+	if (len < 256) {
+	    dv.setUint8(pos++, this.SMALL_ATOM);
+	    dv.setUint8(pos++, len);
+	}
+	else if (len < 65536) {
+	    dv.setUint8(pos++, this.ATOM);
+	    dv.setUint16(pos, len, false);
+	    pos += 2;
+	}	
+	else
+	    throw("bad atom: too big");    
+	pos = this.write_latin1_bytes(name,dv,pos,name.length);
     }
-    else
-	throw("bad atom: too big");
-    return this.unicode_to_bytes(Name,dv,pos,len);
+    return pos;
 }
 
 
@@ -649,7 +867,7 @@ EiClass.prototype.decode_term = function (dv,pos) {
     case this.LARGE_BIG:
 	R = this.decode_big_bytes(dv,pos+4,dv.getUint32(pos,false)); break;
     case this.FLOAT:
-	R = parseFloat(this.bytes_to_string(dv,pos,31)); break;
+	R = parseFloat(this.decode_latin1_string(dv,pos,31)); break;
     case this.NEW_FLOAT:
 	R = dv.getFloat64(pos, false); break;
     case this.REFERENCE:
@@ -659,9 +877,9 @@ EiClass.prototype.decode_term = function (dv,pos) {
     case this.NEWER_REFERENCE:
 	R = this.decode_newer_ref(dv,pos); break;
     case this.STRING:
-	R = this.bytes_to_string(dv,pos+2,dv.getUint16(pos,false)); break;
+	R = this.decode_string(dv,pos+2,dv.getUint16(pos,false)); break;
     case this.LIST:
-	R = this.decode_list(dv,pos); break;
+	R = this.decode_list(dv,pos+4,dv.getUint32(pos,false)); break;
     case this.SMALL_TUPLE:
 	R = this.decode_tuple(dv,pos+1,dv.getUint8(pos)); break;
     case this.LARGE_TUPLE:
@@ -755,30 +973,150 @@ EiClass.prototype.decode_newer_ref = function (dv,pos) {
     return new EiReference(Sys,Refs,Creation);
 };
 
+// check if list of 8-bit / 16-bit integers
+EiClass.prototype.is_string = function (dv,pos,len) {
+    var i = 0;
+    while(i < len) {
+	var tag = dv.getUint8(pos++);
+	if (tag == this.SMALL_INTEGER) {
+	    pos += 1;
+	}
+	else if (tag == this.INTEGER) {
+	    var val = dv.getInt32(pos,false);
+	    if ((val < 0) || (val > 65535)) return false;
+	    pos += 4;
+	}
+	else
+	    return false;
+	i++;
+    }
+    // must end with NIL
+    return dv.getUint8(pos) == this.NIL;
+}
+
+// Convert an array of bytes into a string.
+// STRING <size:16> <utf8-encoded bytes>
+
+EiClass.prototype.decode_string = function (dv,pos,count) {
+    var s = "";
+
+    if (this.decode_string_utf8) {
+	while (count > 0) {
+	    var c1 = dv.getUint8(pos++);
+	    if (c1 < 0x80) {
+		// 7-bit code (ascii)
+		s += String.fromCharCode(c1);
+		count--;
+	    }
+	    else if ((c1 >= 0xc0) && (c1 < 0xe0)) { // 11-bit code
+		var c2 = dv.getUint8(pos++);
+		s += String.fromCharCode(((c1 & 0x1f) << 6) | (c2 & 0x3f));
+		count -= 2;
+	    }
+	    else if ((c1 >= 0xe0) && (c1 < 0xf0)) {
+		// 16-bit code	    
+		var c2 = dv.getUint8(pos++);
+		var c3 = dv.getUint8(pos++);
+		s += String.fromCharCode(((c1 & 0x0f) << 12) |
+					 ((c2 & 0x3f) << 6) | (c3 & 0x3f));
+		count -= 3;
+	    }
+	    else {
+		// this is an error, proably latin1
+		s += String.fromCharCode(c1);
+		count--;
+	    }
+	}
+    }
+    else { // latin1
+	while (count > 0) {
+	    var x = dv.getUint8(pos++);
+	    s += String.fromCharCode(x & 0xff);
+	    count--;
+	}
+    }
+    return s;
+};
+
 //
+// FIXME: find better way
 // Special hack for argument lists  ['array',64,65,66,67]
 // to separate this in the erlang term_to_binary from a string!
 // also make sure [array|T] is encoded as [array,array|T]
 //
-EiClass.prototype.decode_list = function (dv,pos) {
-    var len, i, Arr = [];
 
-    len = dv.getUint32(pos, false);
-    pos += 4;
-    // console.debug("decode_list len=" + len);
-    for (i = 0; i < len; i++) {
-	var Term = this.decode_term(dv,pos);
-	var k   = this.decode_size_term(dv,pos);
-	//console.debug("term (tag="+dv.getUint8(pos)+"["+ Term+ "] size = "+k);
-	pos += k;
-	if (!((i==0) && this.eqAtom(Term, "array")))
-	    Arr.push(Term);
+EiClass.prototype.decode_list = function (dv,pos,count) {
+
+    if (this.decode_list_string && this.is_string(dv,pos,count)) {
+	var s = "";
+
+	while(count > 0) {
+	    var x1;
+	    
+	    if (dv.getUint8(pos++) == this.SMALL_INTEGER)
+	    { x1 = dv.getUint8(pos); pos += 1; }
+	    else { x1 = dv.getInt32(pos,false); pos += 4; }
+	    count--;
+	    
+	    if (this.decode_list_utf8) {
+		if (x1 < 128) {	// 7-bit code (ascii)
+		    s += String.fromCharCode(x1);
+		}
+		else if ((x1 >= 0xc0) && (x1 < 0xe0)) { // 11-bit code
+		    var x2;
+		    
+		    if (dv.getUint8(pos++) == this.SMALL_INTEGER)
+		    { x2 = dv.getUint8(pos); pos += 1; }
+		    else { x2 = dv.getInt32(pos,false); pos += 4; }
+		    count--;
+		    
+		    s += String.fromCharCode(((x1 & 0x1f) << 6) | (x2 & 0x3f));
+		}
+		else if ((x1 >= 0xe0) && (x1 < 0xf0)) {
+		    var x2, x3;
+
+		    if (dv.getUint8(pos++) == this.SMALL_INTEGER)
+		    { x2 = dv.getUint8(pos); pos += 1; }
+		    else { x2 = dv.getInt32(pos,false); pos += 4; }
+		    count--;
+		    
+		    if (dv.getUint8(pos++) == this.SMALL_INTEGER)
+		    { x3 = dv.getUint8(pos); pos += 1; }
+		    else { x3 = dv.getInt32(pos,false); pos += 4; }
+		    count--;
+
+		    s += String.fromCharCode(((x1 & 0x0f) << 12) |
+					     ((x2 & 0x3f) << 6) | (x3 & 0x3f));
+		}
+		else {
+		    // 16 bit or bad latin1 char
+		    s += String.fromCharCode(x1);
+		}
+	    }
+	    else {
+		s += String.fromCharCode(x1);
+	    }
+	}
+	return s;
     }
-    // console.debug("decode_list end");
-    if (dv.getUint8(pos) != this.NIL) {  // improper list not allowed
-	throw ("List does not end with NIL! [tag="+dv.getUint8(pos)+"]");
+    else {
+	var Arr = [];
+	var i;
+	// console.debug("decode_list len=" + len);
+	for (i = 0; i < count; i++) {
+	    var Term = this.decode_term(dv,pos);
+	    var k   = this.decode_size_term(dv,pos);
+	    //console.debug("term (tag="+dv.getUint8(pos)+"["+ Term+ "] size = "+k);
+	    pos += k;
+	    if (!((i==0) && this.eqAtom(Term, "array")))
+		Arr.push(Term);
+	}
+	// console.debug("decode_list end");
+	if (dv.getUint8(pos) != this.NIL) {  // improper list not allowed
+	    throw ("List does not end with NIL! [tag="+dv.getUint8(pos)+"]");
+	}
+	return Arr;
     }
-    return Arr;
 };
 
 EiClass.prototype.decode_tuple = function (dv,pos,len) {
@@ -839,28 +1177,20 @@ EiClass.prototype.utf8_to_string = function (dv,pos,count) {
     return s;
 };
 
-
-// Convert an array of bytes into a string.
-EiClass.prototype.bytes_to_string = function (dv,pos,count) {
+// convert latin1 string to array of bytes
+EiClass.prototype.decode_latin1_string = function (dv,pos,count) {
     var i, s = "";
     for (i = 0; i < count; i++,pos++)
 	s += String.fromCharCode(dv.getUint8(pos));
     return s;
 };
 
+
 // Convert an array of bytes into a string.
 EiClass.prototype.string_to_bytes = function (Obj,dv,pos,count) {
     var i;
     for (i = 0; i < count; i++, pos++)
 	dv.setUint8(pos, Obj.charCodeAt(i));
-    return pos;
-};
-
-// write string as utf8 bytes (FIXME!)
-EiClass.prototype.unicode_to_bytes = function (Obj,dv,pos,count) {
-    var i;
-    for (i = 0; i < count; i++, pos++)
-	dv.setUint8(pos, 127 & Obj.charCodeAt(i));
     return pos;
 };
 
---
+++ src/wse_text_demo.erl
@@ -0,0 +1,31 @@
+-module(wse_text_demo).
+-export([run/2]).
+
+run(Ws, [A,B]) ->
+    {ok,Ap} = wse:getElementById(Ws, A),
+    {ok,Bp} = wse:getElementById(Ws, B),
+    loop(Ws, Ap, Bp).
+
+loop(Ws, Ap, Bp) ->
+    timer:sleep(3000),
+    set_p_text(Ws, Ap, "en", "Good bye!"),
+    set_p_text(Ws, Bp, "en", "Sigur Rós is playing"),
+    timer:sleep(3000),
+    set_p_text(Ws, Ap, "en", "Film time!"),
+    set_p_text(Ws, Bp, "jap", "宮崎駿 is playing"),
+    timer:sleep(3000),
+    set_p_text(Ws, Ap, "en", "Film time2!"),
+    set_p_text(Ws, Bp, "ja", [229,174,174,229,180,142,233,167,191]++" is playing"),
+    timer:sleep(3000),
+    set_p_text(Ws, Ap, "en", "Hello, world!"),
+    set_p_text(Ws, Bp, "en", "Röyksopp is playing"),
+    loop(Ws, Ap, Bp).
+
+set_p_text(Ws, P, Lang, Text) ->
+    io:format("set_text: ~p\n", [Text]),
+    io:format("        : ~w\n", [Text]),
+    wse:set(Ws, P, "lang", Lang),
+    {ok, TextNode} = wse:firstChild(Ws, P),  %% Text node!?
+    wse:set(Ws, TextNode, "nodeValue", Text).
+
+     
